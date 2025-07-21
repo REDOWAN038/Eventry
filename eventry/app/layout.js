9ccbfd3964d32a5e709f897b75d7bd710658c535
REDOWAN038 <redowanibrahim2724@gmail.com>
2025-07-22 00:39:51 +0600

    initial setup

---
+++ eventry/app/layout.js
@@ -0,0 +1,22 @@
+import { Inter } from "next/font/google"
+import "./globals.css"
+import Navbar from "@/components/Navbar"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+    title: "Eventry - Home",
+    description:
+        "A single entry to connected to all the online events from the globe.",
+}
+
+export default async function RootLayout({ children }) {
+    return (
+        <html lang='en'>
+            <body className={inter.className}>
+                <Navbar />
+                <main className='py-8'>{children}</main>
+            </body>
+        </html>
+    )
+}
