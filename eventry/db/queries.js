60b34d73fe586a9fcd294d57fed4a38afd30adce
REDOWAN038 <redowanibrahim2724@gmail.com>
2025-07-23 22:44:53 +0600

    authentication

--- eventry/db/queries.js
+++ eventry/db/queries.js
@@ -1,4 +1,6 @@
 import { eventModel } from "@/models/event-models"
+import { userModel } from "@/models/user-model"
+
 import {
     replaceMongoIdInArray,
     replaceMongoIdInObject,
@@ -14,4 +16,16 @@ async function getEventById(eventId) {
     return replaceMongoIdInObject(event)
 }
 
-export { getAllEvents, getEventById }
+async function createUser(user) {
+    return await userModel.create(user)
+}
+
+async function findUserByCredentials(credentials) {
+    const user = await userModel.findOne(credentials).lean()
+    if (user) {
+        return replaceMongoIdInObject(user)
+    }
+    return null
+}
+
+export { getAllEvents, getEventById, createUser, findUserByCredentials }
